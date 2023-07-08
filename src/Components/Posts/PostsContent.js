a35d1b7203204da9212dc671ae036b0c2bccea0a
tuananhnguyen99 <tuananh6032@gmail.com>
2023-07-08 16:09:57 +0700

    update 08/07 lan2

--- src/Components/Posts/PostsContent.js
+++ src/Components/Posts/PostsContent.js
@@ -7,7 +7,7 @@ function PostsContent() {
     return (
         <>
             <div className=" w-full p-2 pt-0">
-                <span className="block min-h-[50px] px-2">
+                <span className="block text-[14px] lg:text-[15px] min-h-[50px] px-2">
                     Đang có sự cố kết nối xảy ra làm game không thể tạo trận. Bộ
                     phận kỹ thuật đang tiến hành kiểm tra và khắc phục. Các anh
                     hùng xin thử lại sau nhé.
