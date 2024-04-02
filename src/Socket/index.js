186d11727d56ba41d58674cad61ccbf867167aa0
tuananhnguyen99 <tuananh6032@gmail.com>
2024-04-02 17:16:51 +0700

    update

--- src/Socket/index.js
+++ src/Socket/index.js
@@ -10,7 +10,7 @@ function SocketProvider({ children }) {
     useEffect(() => {
         const connect = (userId) => {
             const ws = new WebSocket(
-                `ws://facebook.tuannguyen.click:8080?userId=${userId}`
+                `wss://facebook.tuannguyen.click:8080?userId=${userId}`
             );
             setSocket(ws);
         };
